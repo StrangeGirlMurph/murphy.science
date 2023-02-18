931c3d82d43a308f6dd1d917748b835d046b56ca
Murphy <git@murphy-in.space>
2023-02-18 17:54:45 +0100

    time for some stupid clog debugging

--- src/routes/cats/+page.server.ts
+++ src/routes/cats/+page.server.ts
@@ -3,6 +3,8 @@ import path from 'path';
 
 export async function load() {
     const raw = readFileSync(path.join(process.cwd(), '/static/infinite-cat-supply.json'), "utf-8");
+    console.log(raw);
     const data = JSON.parse(raw);
+    console.log(data);
     return data[Math.floor(Math.random()*data.length)];
 }
