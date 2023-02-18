f58c21c0e7f98dfbedfbf306d7c39528b1f9986f
Murphy <git@murphy-in.space>
2023-02-18 17:52:45 +0100

    maybe fix the bug?

--- src/routes/cats/+page.server.ts
+++ src/routes/cats/+page.server.ts
@@ -1,7 +1,8 @@
 import {readFileSync} from 'fs';
+import path from 'path';
 
 export async function load() {
-    const raw = readFileSync('static/infinite-cat-supply.json', "utf-8");
+    const raw = readFileSync(path.join(process.cwd(), '/static/infinite-cat-supply.json'), "utf-8");
     const data = JSON.parse(raw);
     return data[Math.floor(Math.random()*data.length)];
 }
