ea9bd191c03637e39d61b6590168dc62ec2b5587
Murphy <git@murphy-in.space>
2023-02-18 18:02:17 +0100

    another try

--- src/routes/cats/+page.server.ts
+++ src/routes/cats/+page.server.ts
@@ -2,9 +2,7 @@ import {readFileSync} from 'fs';
 import path from 'path';
 
 export async function load() {
-    const raw = readFileSync(path.join(process.cwd(), '/static/infinite-cat-supply.json'), "utf-8");
-    console.log(raw);
+    const raw = readFileSync(path.join(process.cwd(), 'static/infinite-cat-supply.json'), "utf-8");
     const data = JSON.parse(raw);
-    console.log(data);
     return data[Math.floor(Math.random()*data.length)];
 }
