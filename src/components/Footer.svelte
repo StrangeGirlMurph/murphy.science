c3880adb67680ba629a09db23e5aa25bc47e455a
Murphy <git@murphy-in.space>
2023-04-12 17:11:24 +0200

    spelling + github sponsor

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,7 @@
+<script>
+	import GitHub from 'svelte-material-icons/Github.svelte';
+</script>
+
 <footer
 	class="flex flex-col-reverse sm:flex-row gap-2 justify-between content-center sm:items-center mt-4 w-full"
 >
@@ -9,7 +13,13 @@
 		<div class="text-gray-500 text-xs">Copyright © 2023 Murphy</div>
 	</div>
 
-	<a class="hover:animate-bouncy" href="https://ko-fi.com/murph">
+	<!-- <a class="hover:animate-bouncy" href="https://ko-fi.com/murph">
 		<img class="h-9" src="/kofi.webp" alt="Ko-fi Support Button" />
+	</a> -->
+	<a
+		class="hover:animate-bouncy flex items-center gap-[6px] bg-gradient-to-r from-[#ff4ea3] to-[#ff5e5c] py-[7px] px-[10px] text-l font-semibold text-white rounded-lg"
+		href="https://github.com/sponsors/StrangeGirlMurph"
+	>
+		Support me on GitHub <GitHub class="h-6 w-6 inline-block" />
 	</a>
 </footer>
--- src/routes/university/+page.svelte
+++ src/routes/university/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Links from '../../components/Links.svelte';
+</script>
+
 <svelte:head>
 	<title>University | Murphy</title>
 	<meta name="description" content="University related infos." />
@@ -8,23 +12,42 @@
 	<h2 class="subtitle">Some resources I want to share with my fellow students.</h2>
 </div>
 
-<h1>More things are coming!</h1>
+<h1>Scientific Knowledge Base</h1>
 <section>
 	<p>
-		I am currently working on some big things I want to share with everyone out there (although they
-		will probably mostly be useful for the students at my university). I will update this page as
-		soon as I have something to show!
+		During my studies I take a lot of notes. All these notes and everything else I learn which I
+		also written down somewhere ends up in my knowledge base. It's an <a
+			class="link"
+			href="http://obsidian.md">Obsidian</a
+		>
+		vault written in German which I share with everyone over a GitHub repo under a
+		<a class="link" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+			CC BY-NC-SA 4.0 License</a
+		>. The vault is basically my own little Wikipedia with all the definitions, concepts, theorems
+		and so on I learn. Because it's all written in German mostly my fellow students from Uni and
+		maybe some other German speaking people will find it useful and get something out of it. I hope
+		to some day be able to publish it on the internet in a more accessible way with Obsidian Publish
+		for example.
 	</p>
+	<Links
+		links={[
+			{
+				name: `Knowledge-Base | Repository`,
+				type: `github`,
+				href: `https://github.com/StrangeGirlMurph/Knowledge-Base`
+			}
+		]}
+	/>
 </section>
 
 <h1>History</h1>
 <section>
 	<p>
-		I am currently studying "Computer Science, Mathematics & Physics" in my second semester. It's a
-		tough combination of all three disciplines. I am having the same courses as the regular
-		math/physics/cs students. Just not all of them and my bachelor is longer. Before I started my
-		new bachelor I studied Computer Science & Computational Science somewhere else for a year. It
-		allowed me to take courses on physics and cognitive neurosciences but it was just to applied for
-		me and tbh just to easy. Plus I wanted more and serious math. Not just math for cs.
+		I am currently studying Computer Science, Mathematics & Physics. It's a tough combination of all
+		three disciplines. I am having the same courses as the regular math/physics/cs students. Just
+		not all of them and my bachelor is longer. Before I started this bachelor I studied Computer
+		Science & Computational Science somewhere else for a year. It allowed me to take courses on
+		physics and cognitive neurosciences but it was just too applied for me and tbh too easy. Plus I
+		wanted more and serious math. Not just math for cs.
 	</p>
 </section>
