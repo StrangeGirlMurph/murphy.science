5d9581fdab79187e4bea56400e1e184a068a0927
Murphy <git@murphy-in.space>
2023-05-24 15:30:19 +0200

    style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,9 +2,7 @@
 	import GitHub from 'svelte-material-icons/Github.svelte';
 </script>
 
-<footer
-	class="flex flex-col-reverse sm:flex-row gap-2 justify-between content-center sm:items-center mt-4 w-full"
->
+<footer class="flex flex-row gap-2 justify-between content-center sm:items-center mt-4 w-full">
 	<div class="space-y-1">
 		Made by me the
 		<a href="https://github.com/StrangeGirlMurph" class="text-fav hover:underline">
@@ -16,9 +14,10 @@
 	</div>
 
 	<a
-		class="hover:animate-bouncy w-fit flex items-center justify-center gap-[6px] bg-gradient-to-r from-[#ff4ea3] to-[#ff5e5c] py-[7px] px-[10px] text-l font-semibold text-white rounded-lg"
+		class="hover:animate-bouncy w-fit h-fit flex items-center justify-center gap-[6px] bg-gradient-to-r from-[#ff4ea3] to-[#ff5e5c] py-[7px] px-[10px] text-l font-semibold text-white rounded-lg"
 		href="https://github.com/sponsors/StrangeGirlMurph"
 	>
-		Support me on GitHub <GitHub class="h-6 w-6 inline-block" />
+		<span class="hidden sm:block">Support me on GitHub</span>
+		<GitHub class="h-6 w-6 inline-block" />
 	</a>
 </footer>
