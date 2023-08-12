f1d79fca8bcfc3ad7af7303d8856b13ce0a89117
Murphy <git@murphy-in.space>
2023-08-12 10:56:44 +0200

    width fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div class="flex flex-col items-center" style="min-height:100vh; min-height: 100svh;">
-	<div class="h-[5px] rainbow-bg w-screen" />
+	<div class="h-[5px] rainbow-bg w-full" />
 	<div
 		class="p-4 sm:p-6 flex flex-col flex-grow justify-between items-center w-full max-w-screen-2xl "
 	>
