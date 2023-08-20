1c54ab7ac4fdd0042ce7996d6231f42c8a08c1d6
Murphy <git@murphy-in.space>
2023-08-20 10:59:57 +0200

    github api

--- src/routes/work/+page.ts
+++ src/routes/work/+page.ts
@@ -1,10 +1,10 @@
 /** @type {import('./$types').PageLoad} */
 export async function load() {
 	const res = await fetch(
-		'https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugin-stats.json'
+		'https://api.github.com/repos/StrangeGirlMurph/obsidian-wikipedia-search/releases'
 	);
 
 	const json = await res.json().catch((err) => undefined);
 	if (!json) return {downloads: undefined};
-	return {downloads: json['wikipedia-search']['downloads']};
+	return {downloads: json.map((e: any) => e.assets[0].download_count ?? 0).reduce((a: number, b: number) => a + b)};
 }
